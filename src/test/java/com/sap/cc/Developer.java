f0adf033b98aea2174df5e396e430e5b76fc3914
I503325 <klaus.mueller01@sap.com>
2024-01-10 22:32:28 -0300

    ASE test

---
+++ src/test/java/com/sap/cc/Developer.java
@@ -0,0 +1,48 @@
+package com.sap.cc;
+
+import com.sap.cc.handsoff.CodeCreator;
+import com.sap.cc.handsoff.UnsupportedDevelopmentLanguageException;
+
+public class Developer extends CodeCreator {
+    private String name;
+    private String language;
+
+
+    public Developer(String name, String language) {
+        this.name = name;
+        this.language = language;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    @Override
+    public String code() throws UnsupportedDevelopmentLanguageException {
+        if(this.language == "go"){
+            return String.format("fmt.Println(\"Hello, %s!\")", this.name);
+        }
+
+        if(this.language == "nodejs"){
+            return String.format("console.log(\"Hello, %s!\")",this.name);
+        }
+
+        if(this.language == "python"){
+            return String.format("print(\"Hello, %s!\")",this.name);
+        }
+        throw new UnsupportedDevelopmentLanguageException(this.language);
+    }
+
+}
---
+++ src/test/java/com/sap/cc/Hackathon.java
@@ -0,0 +1,27 @@
+package com.sap.cc;
+
+import com.sap.cc.handsoff.CodeCreator;
+import com.sap.cc.handsoff.DeveloperEvent;
+import com.sap.cc.handsoff.UnsupportedDevelopmentLanguageException;
+
+import java.util.List;
+
+public class Hackathon implements DeveloperEvent {
+
+
+
+    @Override
+    public String codeTogether(List<CodeCreator> codeCreators) {
+        StringBuilder result = new StringBuilder();
+
+        codeCreators.forEach(codeCreator -> {
+            try {
+                String code = codeCreator.code();
+                result.append(code).append("\n");
+            } catch (UnsupportedDevelopmentLanguageException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        return result.toString();
+    }
+}
